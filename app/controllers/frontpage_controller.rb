94eaf983b46a3cf88432a4f3fddd004b727fc46c
Gestión de la Educación Autónoma (GEA) <barraorion@gmail.com>
2011-07-16 10:47:15 +0200

    first skeleton

---
+++ app/controllers/frontpage_controller.rb
@@ -0,0 +1,10 @@
+class FrontpageController < ApplicationController
+  
+  
+  # Webfinger protocol
+  # http://code.google.com/p/webfinger/
+  def host_meta
+  end
+  
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeaSimpleWeb::Application.config.secret_token = 'e976418c5138e8f7f4386889d1ea8b5d578f7a479991d2ce7d441b2737f443649c0fc44cd01267bf14d0cd81e71e775c7c4a3b1fbe8d7787caf93575495309b3'
