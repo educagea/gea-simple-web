d9c5ab7e80e4f2cb56676e46eda9f4c3851f3c51
Gestión de la Educación Autónoma (GEA) <barraorion@gmail.com>
2011-09-01 21:32:54 +0200

    new version of frontpage

---
+++ app/assets/javascripts/frontpage.js
@@ -0,0 +1,26 @@
+//javascript to show/hide elements in the frontpage
+
+show_info_in_frontpage = function(position, item_number){
+	switch(position)
+	{
+	case "left":
+	  $(".left_content").hide();
+	  $("#info_item_"+item_number).addClass("left_content");
+	  $("#half_left").append($("#info_item_"+item_number));
+	  $("#info_item_"+item_number).show();
+	  break;
+	case "right":
+	  $(".right_content").hide();
+	  $("#info_item_"+item_number).addClass("right_content");
+	  $("#half_right").append($("#info_item_"+item_number));
+	  $("#info_item_"+item_number).show();
+	  break;
+	}
+};
+
+back_to_main = function(){
+	$(".left_content").hide();
+	$(".right_content").hide();
+	$("#main_left").show();
+	$("#main_right").show();
+};
