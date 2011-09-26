65728ab8110de51e9fc8a88f70a76da3f0b7964c
Gestión de la Educación Autónoma (GEA) <barraorion@gmail.com>
2011-09-26 14:22:20 +0200

    page names in spanish

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ GeaSimpleWeb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "frontpage#home"
+  root :to => "frontpage#inicio"
 
   # See how all your routes lay out with "rake routes"
   match ':action' => 'frontpage#:action'
