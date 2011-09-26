dcb2761fe8d048f56899d01e1fe39869a64d469f
Gestión de la Educación Autónoma (GEA) <barraorion@gmail.com>
2011-09-26 11:27:22 +0200

    setup for sending emails 3

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "mundogea.com",
-  :user_name            => "enrique.barra",
+  :user_name            => "enrique.barra@mundogea.com",
   :password             => ENV['EMAIL_PASS'],
-  :authentication       => :login,
+  :authentication       => "plain",
   :enable_starttls_auto => true
 }
